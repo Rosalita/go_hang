52afa5b49e203d3f9e9bd4c8038868aabb2087a3
Rosie Hamilton <enchantrix@gmail.com>
2015-11-10 22:11:29 +0000

    each guess now tested against the word and string of ---- is updated as each letters is found

--- hang.go
+++ hang.go
@@ -7,31 +7,48 @@ import(
 
 func main(){
   stage_of_death := 0
+  has_guessed_1_letter := false
   guess :=""
   guessed_letters :=""
+  var dashes string
+  var newdashes string
   word :="test" //to do make a long list of words and pick one at random.
   fmt.Printf("H A N G M A N\n")
   for {
       draw_hangman(stage_of_death)
-      draw_dashes(len(word))
+      if has_guessed_1_letter == false{
+          dashes = hideword(len(word))
+          fmt.Printf("%s\n",dashes)
+      } else {
+          fmt.Printf("%s\n", newdashes)
+      }
       fmt.Printf("Guess a letter: ")
       fmt.Scanln(&guess)
+
       isALetter, _ := regexp.MatchString("^[a-zA-Z]",guess)
       if isALetter == false{
-        fmt.Printf("That's not a letter! Try again\n")
+          fmt.Printf("That's not a letter! Try again\n")
       } else if (len(guess) > 1){
-        fmt.Printf("You entered more than 1 character! Try again\n")
+          fmt.Printf("You entered more than 1 character! Try again\n")
       } else if strings.Contains(guessed_letters, guess){
           fmt.Printf("You have already guessed that letter! Try again\n")
       } else if strings.Contains(word, guess){
           fmt.Printf("The letter you guessed is in the word\n")
           guessed_letters += guess
-          fmt.Printf(guessed_letters)
+
+          if has_guessed_1_letter == false{
+              updateddashes:= revealdashes(word, guess, dashes)
+              newdashes = updateddashes
+          } else {
+              updateddashes:= revealdashes(word,guess,newdashes)
+              newdashes = updateddashes
+          }
+          
+          has_guessed_1_letter = true
       } else {
           fmt.Printf("The letter you guessed is not in the word\n")
           stage_of_death ++
           guessed_letters += guess
-          fmt.Printf(guessed_letters)
       }
   }
 }
@@ -108,10 +125,29 @@ func draw_hangman(stage_of_death int){
             fmt.Printf("========\n")
     }
 }
-func draw_dashes(number_of_dashes int){
+func hideword(wordlen int) string {
+    var dashes string
     var i int
-    for i = 0; i < number_of_dashes; i++{
-        fmt.Printf("_")
+    for i = 0; i < wordlen; i++{
+        dashes += "_"
     }
-    fmt.Printf("\n")
+    return dashes
+}
+func revealdashes(word string, guess string, dashes string) string{
+    var newdashes string
+    for i, r := range dashes {
+        c := string(r)
+        if c != "_"{
+            newdashes += c
+
+        } else {
+            var letter = string(word[i])
+                if guess == letter{
+                    newdashes += guess
+                } else {
+                   newdashes += "_"
+                }
+        }
+    }
+    return newdashes
 }
